69721f0c1c03a6a1c6198fac7fc5ff7e89523cda
William Wilkins <williamwilkins@trentu.ca>
2018-01-08 18:26:22 -0500

    Done!

---
+++ src/myclass.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class myclass {
+	public static void main(String[] args){
+		int i;
+		String st;
+		
+		// User input
+		Scanner s = new Scanner(System.in);
+		System.out.print("Give a string: ");
+		st = s.nextLine();
+		
+		System.out.println("\nString is " + st);
+		
+		// Loop
+		for (i = 0; i < st.length(); i++){
+			System.out.print("'" + st.charAt(i) + "'");
+			if(i < st.length() - 1)
+				System.out.print(" + ");
+		}
+		
+		// Conditional
+		if(i>5){
+			System.out.println("\nString is big (" + i + ")");
+		} 
+		else {
+			System.out.println("\nString is small (" + i + ")");
+		}
+		
+	}
+}
